a30a5c94249b827c95844bebb1913ae6f6d250fb
Mathias Gumz <mg@2hoch5.com>
2007-05-16 03:58:51 +0200

    typo

--- templates/skel.sh
+++ templates/skel.sh
@@ -2,7 +2,7 @@
 #
 #############################################################
 #
-# file    : @FILE@.rb
+# file    : @FILE@.sh
 # author  : mathias gumz <akira at fluxbox dot org>
 # copyr   : copyright (c) @YEAR@ by m. gumz
 # license : see LICENSE
