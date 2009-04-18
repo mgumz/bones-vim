39cdf70ea56bc8dce97d3957e8b7aec579d81f1c
Mathias Gumz <mg@2hoch5.com>
2009-04-18 08:52:39 +0200

    fixed some bugs in creating the qt4-tags on english windows

--- tags/create_qt4_tags_win.cmd
+++ tags/create_qt4_tags_win.cmd
@@ -1,4 +1,4 @@
-( for /R %QTDIR%\include %%h in (*.h) do @type %%h ) > header.lst
-%VIMRUNTIME%\vim.exe -u NONE -U NONE -N -s create_qt4_tags.vim header.lst
-%VIMRUNTIME%\ctags.exe -L header.lst -f qt4.tags --c++-kinds=+p --fields=+iaS --extra=+q
-del header.lst
+( for /R "%QTDIR%\include" %%h in (*.h) do @type %%h ) > "%TEMP%\header.lst"
+"%VIMRUNTIME%"\vim.exe -u NONE -U NONE -N -s "%~dp0create_qt4_tags_win.vim" "%TEMP%\header.lst"
+"%VIMRUNTIME%"\ctags.exe -L "%TEMP%\header.lst" -f qt4.tags --c++-kinds=+p --fields=+iaS --extra=+q
+del "%TEMP%\header.lst"
